05b8007ad09d0bc82c7432b5e39fe9e740d5aa05
rur0 <ruslansromanovs@gmail.com>
2021-02-25 05:06:12 +0000

    added concurrent-safe TokenPricesCache access

--- cache.go
+++ cache.go
@@ -1,5 +1,7 @@
 package coingecko
 
+import "sync"
+
 // PricesCache to use globally
 var PricesCache = Prices{}
 
@@ -10,12 +12,43 @@ func UpdatePriceCache(coins []string, currencies []string) error {
 	return err
 }
 
+type TokenPricesCache struct {
+	prices TokenPrices
+	sync.RWMutex
+}
+
+func (tp TokenPricesCache) GetPrice(contract string, currency string) (float64, bool) {
+	tp.RLock()
+	defer tp.RUnlock()
+	contractPrices, ok := tp.prices[contract]
+	if !ok {
+		return 0, false
+	}
+
+	price, ok := contractPrices[currency]
+	if !ok {
+		return 0, false
+	}
+
+	return price, ok
+}
+
+func (tp *TokenPricesCache) SetPrices(prices TokenPrices) {
+	tp.Lock()
+	tp.prices = prices
+	tp.Unlock()
+}
+
 // TokenPricesCache to use globally
-var TokenPricesCache = TokenPrices{}
+var TokenPricesCached = TokenPricesCache{}
 
 // UpdateTokenPriceCache updates the token price cache to use globally
 func UpdateTokenPriceCache(contractAddrs []string, currencies []string) error {
 	var err error
-	TokenPricesCache, err = GetTokenPrices(contractAddrs, currencies)
-	return err
+	prices, err := GetTokenPrices(contractAddrs, currencies)
+	if err != nil {
+		return err
+	}
+	TokenPricesCached.SetPrices(prices)
+	return nil
 }
